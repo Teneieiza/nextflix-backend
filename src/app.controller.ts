adbd8f7f934374a9c68d51d636276265423891aa
tenmonster <tenmonster@hotmail.com>
2025-02-02 18:42:08 +0700

    Add Route Genre & Content

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  getHello(): any {
+    throw new Error('Method not implemented.');
+  }
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getRoot() {
+    return { message: 'Welcome to NestJS API' };
+  }
+
+  @Get('genre/:genreCode')
+  async getGenre(@Param('genreCode') genreCode: string) {
+    return this.appService.getGenre(Number(genreCode));
+  }
+
+  @Get('content/:contentId')
+  async getContent(@Param('contentId') contentId: string) {
+    return this.appService.getContent(Number(contentId));
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { ConfigService } from './config/config.service';
+import { GenreType } from './type/genre.type';
+import { ContentType } from './type/content.type';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly configService: ConfigService) {}
+
+  async getGenre(genreCode: number): Promise<GenreType> {
+    const options = {
+      method: 'GET',
+      url: `https://${this.configService.get('XRAPIDAPIHOST')}/genre`,
+      params: { genrecode: genreCode },
+      headers: {
+        'x-rapidapi-key': this.configService.get('XRAPIDAPIKEY'),
+        'x-rapidapi-host': this.configService.get('XRAPIDAPIHOST'),
+      },
+    };
+
+    const response = await axios.request<GenreType>(options);
+    return response.data;
+  }
+
+  async getContent(contentId: number): Promise<ContentType> {
+    const options = {
+      method: 'GET',
+      url: `https://${this.configService.get('XRAPIDAPIHOST')}/content`,
+      params: { contentId: contentId },
+      headers: {
+        'x-rapidapi-key': this.configService.get('XRAPIDAPIKEY'),
+        'x-rapidapi-host': this.configService.get('XRAPIDAPIHOST'),
+      },
+    };
+
+    const response = await axios.request<ContentType>(options);
+    return response.data;
   }
 }
